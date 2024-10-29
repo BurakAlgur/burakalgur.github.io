26b94e6b77966a12d374ffd5eadec9e6392f53a0
burak algur <burak.algur98@gmail.com>
2024-10-29 23:43:44 +0300

    angular and nextjs added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -14,6 +14,8 @@ import Sql from "../assets/skills/sql.png";
 import Mongo from "../assets/skills/mongo.png";
 import Docker from "../assets/skills/docker.png";
 import Git from "../assets/skills/git.png";
+import Angular from "../assets/skills/angular.png";
+import Next from "../assets/skills/next.webp";
 import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
@@ -183,6 +185,29 @@ const Skills = () => {
             </div>
           </div>
         </div>
+        <div className="group relative flex cursor-pointer">
+          <img
+            src={Angular}
+            alt="Angular"
+            className="object-contain w-24 h-24 filter group-hover:blur-md transition duration-300 ease-in-out"
+            style={{ opacity: 1, transform: "none" }}
+          />
+          <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out w-24 h-24 z-0 ">
+            <div className="flex items-center justify-center h-full">
+              <p className="smallScreen:text-md smallScreen:font-medium md:text-2xl font-bold lg:text-3xl text-white opacity-100 text-right">
+                <div className="w-20 h-20">
+                  <CircularProgressbar
+                    value={30}
+                    styles={buildStyles({
+                      pathColor: "darkorchid",
+                      trailColor: "white",
+                    })}
+                  />
+                </div>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="flex flex-row justify-evenly	 mb-10 items-center">
@@ -245,6 +270,29 @@ const Skills = () => {
                 <div className="w-20 h-20">
                   <CircularProgressbar
                     value={65}
+                    styles={buildStyles({
+                      pathColor: "darkorchid",
+                      trailColor: "white",
+                    })}
+                  />
+                </div>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="group relative flex cursor-pointer">
+          <img
+            src={Next}
+            alt="Next"
+            className="object-contain w-24 h-24 filter group-hover:blur-md transition duration-300 ease-in-out"
+            style={{ opacity: 1, transform: "none" }}
+          />
+          <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out w-24 h-24 z-0 ">
+            <div className="flex items-center justify-center h-full">
+              <p className="smallScreen:text-md smallScreen:font-medium md:text-2xl font-bold lg:text-3xl text-white opacity-100 text-right">
+                <div className="w-20 h-20">
+                  <CircularProgressbar
+                    value={55}
                     styles={buildStyles({
                       pathColor: "darkorchid",
                       trailColor: "white",
